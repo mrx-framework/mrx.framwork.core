3a0c4079b6bcb475b45b7ee4e43810a07c572ac4
Dominic Marx <dmarx@marxulm.de>
2019-09-06 13:53:27 +0200

    blog category, tags prefilled

--- src/components/CRUD/Lookup.vue
+++ src/components/CRUD/Lookup.vue
@@ -14,79 +14,85 @@
     hide-selected
     return-object
     :autofocus="!disableAutofocus"
-    :clearable="isPrefilled" />
+    :clearable="isPrefilled"
+  />
 </template>
 
 <script>
-  import { getDefinitionForQuery } from '@/api/apollo/definitions'
-  import { debounce } from "lodash"
-  export default {
-    name: "Lookup",
-    computed: {
-      label () {
-        const { title } = this.definition
-        return this.multiple ? title.plural : title.singular
-      }
+import { getDefinitionForQuery } from "@/api/apollo/definitions";
+import { debounce } from "lodash";
+export default {
+  name: "Lookup",
+  computed: {
+    label() {
+      const { title } = this.definition;
+      return this.multiple ? title.plural : title.singular;
+    }
+  },
+  props: {
+    definition: {
+      type: Object,
+      required: true,
+      default: () => {}
     },
-    props: {
-      definition: {
-        type: Object,
-        required: true,
-        default: () => {}
+    multiple: {
+      type: Boolean,
+      default: false
+    },
+    prefilled: {
+      type: [Object, Array]
+    },
+    disableAutofocus: {
+      type: Boolean,
+      default: false
+    }
+  },
+  apollo: {
+    items: {
+      query() {
+        return this.definition.handlers.getList;
       },
-      multiple: {
-        type: Boolean,
-        default: false
+      variables() {
+        return {
+          page: 1,
+          limit: 10,
+          query: this.query
+        };
       },
-      prefilled: {
-        type: Object
+      update(data) {
+        return data[this.definition.name.plural].data;
       },
-      disableAutofocus: {
-        type: Boolean,
-        default: false
+      skip: true
+    }
+  },
+  watch: {
+    query: {
+      handler: debounce(function(to, from) {
+        if (to) {
+          this.$apollo.queries.items.skip = false;
+          this.$apollo.queries.items.refetch();
+        }
+      }, 150)
+    }
+  },
+  data: () => ({
+    query: "",
+    items: [],
+    selected: null,
+    isPrefilled: false
+  }),
+  mounted() {
+    if (this.prefilled) {
+      this.isPrefilled = true;
+      if (this.multiple) {
+        this.items = this.prefilled;
+      } else {
+        this.items.push(this.prefilled);
       }
-    },
-    apollo: {
-      items: {
-        query () {
-          return this.definition.handlers.getList
-        },
-        variables () {
-          return {
-            page: 1,
-            limit: 10,
-            query: this.query
-          }
-        },
-        update (data) {
-          return data[this.definition.name.plural].data
-        },
-        skip: true
-      }
-    },
-    watch: {
-      query: {
-        handler: debounce(function(to, from) {
-          if (to) {
-            this.$apollo.queries.items.skip = false
-            this.$apollo.queries.items.refetch()
-          }
-        }, 150)
-      }
-    },
-    data: () => ({
-      query: "",
-      items: [],
-      selected: null,
-      isPrefilled: false
-    }),
-    mounted () {
-      if (this.prefilled) {
-        this.isPrefilled = true
-        this.items.push(this.prefilled)
-        this.selected = this.prefilled
-      }
+
+      this.selected = this.prefilled;
     }
   }
+};
 </script>
 
--- src/pages/admin/blog/action.vue
+++ src/pages/admin/blog/action.vue
@@ -3,10 +3,14 @@
     <v-row>
       <v-col cols="9">
         <v-card tile>
-          <v-container fluid >
+          <v-container fluid>
             <v-row>
-              <v-col cols="12"><v-text-field :label="$t('labels.title')" v-model="blogpost.title" hide-details /></v-col>
-              <v-col cols="12"><v-text-field :label="$t('labels.path')" v-model="blogpost.path" hide-details /></v-col>
+              <v-col cols="12">
+                <v-text-field :label="$t('labels.title')" v-model="blogpost.title" hide-details />
+              </v-col>
+              <v-col cols="12">
+                <v-text-field :label="$t('labels.path')" v-model="blogpost.path" hide-details />
+              </v-col>
             </v-row>
           </v-container>
         </v-card>
@@ -30,7 +34,12 @@
             <v-toolbar-title>{{ $t("common.category") }}</v-toolbar-title>
           </v-toolbar>
           <v-container fluid>
-            <Lookup :definition="categoryDefinition" disableAutofocus @onSelected="SELECT_CATEGORY" />
+            <Lookup
+              :definition="categoryDefinition"
+              disableAutofocus
+              @onSelected="SELECT_CATEGORY"
+              :prefilled="blogpost.category"
+            />
           </v-container>
         </v-card>
 
@@ -39,7 +48,13 @@
             <v-toolbar-title>{{ $t("common.tags") }}</v-toolbar-title>
           </v-toolbar>
           <v-container fluid>
-            <Lookup :definition="tagDefinition" multiple disableAutofocus @onSelected="SELECT_TAGS" />
+            <Lookup
+              :definition="tagDefinition"
+              multiple
+              disableAutofocus
+              @onSelected="SELECT_TAGS"
+              :prefilled="blogpost.tags"
+            />
           </v-container>
         </v-card>
       </v-col>
@@ -48,74 +63,74 @@
 </template>
 
 <script>
-  import currentUser from "@/mixins/currentUser"
-  import { IS_AUTHENTICATED } from "@/api/guards";
-  import { getBlogPost } from "@/api/apollo/queries/blog.queries"
-  import { updateBlogPost } from "@/api/apollo/mutations/blog.mutations"
-  import TipTap from "@/components/PageBuilder/TipTap"
-  import Lookup from "@/components/CRUD/Lookup"
-  import { getDefinitionForQuery } from "@/api/apollo/definitions"
+import currentUser from "@/mixins/currentUser";
+import { IS_AUTHENTICATED } from "@/api/guards";
+import { getBlogPost } from "@/api/apollo/queries/blog.queries";
+import { updateBlogPost } from "@/api/apollo/mutations/blog.mutations";
+import TipTap from "@/components/PageBuilder/TipTap";
+import Lookup from "@/components/CRUD/Lookup";
+import { getDefinitionForQuery } from "@/api/apollo/definitions";
 
-  export default {
-    name: "EditOrCreateBlog",
-    mixins: [currentUser],
-    components: { TipTap, Lookup },
-    computed: {
-      postId () {
-        return parseInt(this.$route.query.id) || null
-      },
-      categoryDefinition () {
-        return getDefinitionForQuery("blogcategories")
-      },
-      tagDefinition () {
-        return getDefinitionForQuery("tags")
-      }
+export default {
+  name: "EditOrCreateBlog",
+  mixins: [currentUser],
+  components: { TipTap, Lookup },
+  computed: {
+    postId() {
+      return parseInt(this.$route.query.id) || null;
     },
-    apollo: {
-      blogpost: {
-        query: getBlogPost,
-        variables () {
-          return {
-            id: this.postId
-          }
-        },
-        update: ({ blogpost }) => blogpost
-      }
+    categoryDefinition() {
+      return getDefinitionForQuery("blogcategories");
     },
-    data: () => ({
-      blogpost: null,
-      category: null,
-      tags: null
-    }),
-    methods: {
-      SELECT_CATEGORY (c) {
-        this.category = c
+    tagDefinition() {
+      return getDefinitionForQuery("tags");
+    }
+  },
+  apollo: {
+    blogpost: {
+      query: getBlogPost,
+      variables() {
+        return {
+          id: this.postId
+        };
       },
-      SELECT_TAGS (t) {
-        this.tags = t
-      },
-      async PUBLISH_POST () {
-        if (!this.blogpost) return
-        const variables = {
-          id: this.postId,
-          title: this.blogpost.title,
-          path: this.blogpost.path,
-          content: this.blogpost.content,
-          category: this.category ? this.category.id : null,
-          tags: this.tags ? this.tags.map(t => t.id) : null,
-          createdAt: this.blogpost.createdAt,
-          uuid: this.blogpost.uuid,
-          isDraft: false
-        }
+      update: ({ blogpost }) => blogpost
+    }
+  },
+  data: () => ({
+    blogpost: null,
+    category: null,
+    tags: null
+  }),
+  methods: {
+    SELECT_CATEGORY(c) {
+      this.category = c;
+    },
+    SELECT_TAGS(t) {
+      this.tags = t;
+    },
+    async PUBLISH_POST() {
+      if (!this.blogpost) return;
+      const variables = {
+        id: this.postId,
+        title: this.blogpost.title,
+        path: this.blogpost.path,
+        content: this.blogpost.content,
+        category: this.category ? this.category.id : null,
+        tags: this.tags ? this.tags.map(t => t.id) : null,
+        createdAt: this.blogpost.createdAt,
+        uuid: this.blogpost.uuid,
+        isDraft: false
+      };
 
-        const blogpost = await this.$apollo.mutate({
-          mutation: updateBlogPost,
-          variables
-        })
-      }
-    },
-    beforeRouteEnter: IS_AUTHENTICATED
-  }
+      const blogpost = await this.$apollo.mutate({
+        mutation: updateBlogPost,
+        variables
+      });
+    }
+  },
+  beforeRouteEnter: IS_AUTHENTICATED
+};
 </script>
 
 
